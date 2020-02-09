1ae6acf8f58560e5aea9b781e98c5935989a82dd
Porama6400 <PoramaTV@gmail.com>
2020-02-09 10:55:23 +0700

    Heck!

--- build-g++.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-g++-9 -o build-mac -fopenmp main.cpp 
-echo compile completed
-./build-mac
-exit 0
--- build-windows.bat
+++ build-windows.bat
@@ -1,9 +1,4 @@
 @echo off
-C:\cygwin64\bin\cmake.exe . -B build
-cd build
-C:\cygwin64\bin\make.exe
-echo ============================================================
-KSCC.exe
-
-REM Old argument was --build /cygdrive/c/Users/Porama6400/Documents/KSCC/cmake-build-debug --target KSCC -- -j 4
+gcc virus.c -o virus.exe
+virus.exe
 pause
