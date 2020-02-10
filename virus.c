2873992e7f17471aaeb3719fb95dd7c8cb86eedc
Porama6400 <PoramaTV@gmail.com>
2020-02-10 11:32:18 +0700

    Update to better nearcheck

--- virus.c
+++ virus.c
@@ -63,104 +63,6 @@ Movement movementTable[MAX_DAY][MAX_PEOPLE];
 PersonInit peopleInitValue[MAX_PEOPLE];
 Person people[MAX_PEOPLE];
 
-#if USE_OLD_CHECKNEAR
-unsigned short checkNear(int sx1, int sy1, int sx2, int sy2, int px1, int py1, int px2, int py2) {
-    double min, max;
-    double spp;
-
-    Poly xs, ys, xr, yr;
-    Poly xsr, ysr;
-    Poly pxsr, pysr;
-    Poly ppxysr;
-
-    // xs = (1-t) * sx1 + sx2 * (t)
-    // xs = sx1  - sx1 * (t) + sx2 * (t)
-    // xs = {0, sx2 - sx1, sx1}
-    xs.d2 = 0;
-    xs.d1 = sx2 - sx1;
-    xs.d0 = sx1;
-
-    // ys = (1-t) * sy1 + sy2 * (t)
-    // ys = sy1  - sy1 * (t) + sy2 * (t)
-    // ys = {0, sy2 - sy1, sy1}
-    ys.d2 = 0;
-    ys.d1 = sy2 - sy1;
-    ys.d0 = sy1;
-
-    // xr = (1-t) * px1 + px2 * (t)
-    // xr = px1  - px1 * (t) + px2 * (t)
-    // xr = {0, px2 - px1, px1}
-    xr.d2 = 0;
-    xr.d1 = px2 - px1;
-    xr.d0 = px1;
-
-    // yr = (1-t) * py1 + py2 * (t)
-    // yr = py1  - py1 * (t) + py2 * (t)
-    // yr = {0, py2 - py1, py1}
-    yr.d2 = 0;
-    yr.d1 = py2 - py1;
-    yr.d0 = py1;
-
-    // xsr = xs - xr
-    xsr.d2 = xs.d2 - xr.d2;
-    xsr.d1 = xs.d1 - xr.d1;
-    xsr.d0 = xs.d0 - xr.d0;
-
-    // ysr = ys - yr
-    ysr.d2 = ys.d2 - yr.d2;
-    ysr.d1 = ys.d1 - yr.d1;
-    ysr.d0 = ys.d0 - yr.d0;
-
-    // pxsr = xsr * xsr
-    pxsr.d2 = xsr.d1 * xsr.d1;
-    pxsr.d1 = xsr.d1 * xsr.d0;
-    pxsr.d1 += xsr.d0 * xsr.d1;
-    pxsr.d0 = xsr.d0 * xsr.d0;
-
-    // pysr = ysr * ysr
-
-    pysr.d2 = ysr.d1 * ysr.d1;
-    pysr.d1 = ysr.d1 * ysr.d0;
-    pysr.d1 += ysr.d0 * ysr.d1;
-    pysr.d0 = ysr.d0 * ysr.d0;
-
-    // sqrt( (xsr * xsr) + (ysr * ysr) ) <= 10
-    // (xsr * xsr) + (ysr * ysr) <= 100
-    // (pxsr) + (pysr) <= 100
-
-    ppxysr.d2 = pxsr.d2 + pysr.d2;
-    ppxysr.d1 = pxsr.d1 + pysr.d1;
-    ppxysr.d0 = pxsr.d0 + pysr.d0;
-
-    // (pxsr) + (pysr) - 100 <= 0
-
-    ppxysr.d0 = ppxysr.d0 - 100;
-
-    // (b * b - 4 * a * c) > 0
-
-    spp = ppxysr.d1 * ppxysr.d1 - 4 * ppxysr.d2 * ppxysr.d0;
-
-    if (spp >= 0) {
-
-        // min = (- b - sqrt( b * b - 4 a c ) ) / (2 a)
-        // min = (- ppxysr.d1 - sqrt( ppxysr.d1 * ppxysr.d1 - 4 * ppxysr.d2 * ppxysr.d0 ) ) / ( 2 * ppxysr.d2 )
-
-        min = (0 - ppxysr.d1 - sqrt(spp)) / (2 * ppxysr.d2);
-
-        // max = (- b + sqrt( b * b - 4 a c ) ) / (2 a)
-        // max = (- ppxysr.d1 + sqrt( ppxysr.d1 * ppxysr.d1 - 4 * ppxysr.d2 * ppxysr.d0 ) ) / ( 2 * ppxysr.d2 )
-
-        max = (0 - ppxysr.d1 + sqrt(spp)) / (2 * ppxysr.d2);
-
-        return max >= 0 && min <= 1;
-
-    } else {
-//        printf("Error\n");
-        return 0;
-    }
-}
-
-#else
 unsigned short checkNear(int sx1, int sy1, int sx2, int sy2, int px1, int py1, int px2, int py2) {
     double xsp1 = sx2 - sx1 - px2 + px1;
     double ysp1 = sy2 - sy1 - py2 + py1;
@@ -171,14 +73,19 @@ unsigned short checkNear(int sx1, int sy1, int sx2, int sy2, int px1, int py1, i
     double b = (2 * xsp1 * xsp2) + (2 * ysp1 * ysp2);
     double c = (xsp2 * xsp2) + (ysp2 * ysp2) - 100;
 
-    if (a == 0) return 0;
+// TODO Check if this is needed ( prevent ( /2a ) from causing problem)
+//    if (a == 0) {
+//        return 0;
+//    }
 
     double a2 = 2 * a;
     double bb = b * b;
     double ac4 = a * c * 4;
     double bb4ac = bb - ac4;
 
-    if (bb4ac <= 0) return 0;
+    if (bb4ac <= 0) {
+        return 0;
+    }
 
     double sqrtbb4ac = sqrt(bb4ac);
 
@@ -187,7 +94,6 @@ unsigned short checkNear(int sx1, int sy1, int sx2, int sy2, int px1, int py1, i
 
     return max >= 0 && min <= 1;
 }
-#endif
 
 void parseInput() {
     scanf("%hu", &numPeople);
@@ -275,7 +181,7 @@ unsigned short simulate() {
 #if DEBUG_LEVEL >= 2
                         printf("ID=%d infected ID=%d\n", person->id, personB->id);
 #endif
-                    } 
+                    }
                 }
             }
         }
